b4a604785b27aff88583e65d7ffa5b5bc41243e3
josecis0216 <josecis0216@gmail.com>
2023-02-22 09:36:39 -0700

    fixed some components on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,51 @@
 <template>
   <section>
     <header-nav />
-    <b-row>
-      <b-col>
-        <b-container>
-          <b-row>
-            <b-col>
-              <div class="intro-section">
-                <h1 class="title-tag">Your Commercial Cleaning Solution</h1>
-                <p class="intro-text">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Nulla sunt nobis id commodi quasi suscipit, voluptatum
-                  exercitationem, quod facilis, voluptas et quam. Sed facilis
-                  debitis numquam. Nemo veniam corporis eos.
-                </p>
-              </div>
-            </b-col>
-          </b-row>
-        </b-container>
-      </b-col>
-      <b-col>
+    <div class="row">
+      <div class="col-sm-12 col-lg-6">
+        <div class="intro-section">
+          <h1 class="title-tag">Your Commercial Cleaning Solution</h1>
+          <p class="intro-text">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla sunt
+            nobis id commodi quasi suscipit, voluptatum exercitationem, quod
+            facilis, voluptas et quam. Sed facilis debitis numquam. Nemo veniam
+            corporis eos.
+          </p>
+        </div>
+      </div>
+      <div class="col-sm-12 col-lg-6">
         <img
           id="home-img"
           src="@/static/images/commercial-b-home.jpg"
           alt="cleaning home page building"
           style="width: 100%; height: 100%"
         />
-      </b-col>
-    </b-row>
-    <b-container>
-      <b-row>
-        <b-col>
-          <img
-          id="home-img2"
-            src="@/static/images/commercial-b-inside.jpg"
-            alt="inside of commercial building"
-            style="width: 100%; height: 100%"
-          />
-        </b-col>
-        <b-col>
-          <b-container>
-            <b-row>
-              <b-col>
-                <div class="intro-section">
-                  <h1 class="title-tag">Peace of Mind Results</h1>
-                  <p class="intro-text">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Nulla sunt nobis id commodi quasi suscipit, voluptatum
-                    exercitationem, quod facilis, voluptas et quam. Sed facilis
-                    debitis numquam. Nemo veniam corporis eos.
-                  </p>
-                </div>
-              </b-col>
-            </b-row>
-          </b-container>
-        </b-col>
-      </b-row>
+      </div>
+    </div>
 
+    <div class="row">
+      <div class="col-sm-12 col-lg-6">
+        <img
+          id="home-img2"
+          src="@/static/images/commercial-b-inside.jpg"
+          alt="inside of commercial building"
+          style="width: 100%; height: 100%"
+        />
+      </div>
+      <div class="col-sm-12 col-lg-6">
+        <div class="intro-section">
+          <h1 class="title-tag">Peace of Mind Results</h1>
+          <p class="intro-text">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla sunt
+            nobis id commodi quasi suscipit, voluptatum exercitationem, quod
+            facilis, voluptas et quam. Sed facilis debitis numquam. Nemo veniam
+            corporis eos.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <b-container>
       <b-row>
         <b-col>
           <div class="flip-card">
@@ -119,6 +108,7 @@
           <h1 style="text-align: center; margin-top: 15px">Areas Serviced</h1>
         </b-col>
       </b-row>
+
       <b-row>
         <b-col>
           <ul class="home-list">
@@ -137,12 +127,14 @@
           </ul>
         </b-col>
       </b-row>
+
       <b-row>
         <b-col>
           <h1>Aurora's Cleaning</h1>
           <p>North Salt Lake, Utah, 84054</p>
         </b-col>
       </b-row>
+
       <b-row>
         <b-col cols="2">
           <p>Monday</p>
@@ -163,9 +155,15 @@
           <p>Closed</p>
         </b-col>
         <b-col cols="8">
-            <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3018.163210632271!2d-111.94409738493127!3d40.846340337323475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8752f72f0c94efb1%3A0x5af0d1e5121aff96!2s241%20Buckingham%20Dr%2C%20North%20Salt%20Lake%2C%20UT%2084054!5e0!3m2!1sen!2sus!4v1676484546855!5m2!1sen!2sus" 
-            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3018.163210632271!2d-111.94409738493127!3d40.846340337323475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8752f72f0c94efb1%3A0x5af0d1e5121aff96!2s241%20Buckingham%20Dr%2C%20North%20Salt%20Lake%2C%20UT%2084054!5e0!3m2!1sen!2sus!4v1676484546855!5m2!1sen!2sus"
+            width="600"
+            height="450"
+            style="border: 0"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
         </b-col>
       </b-row>
     </b-container>
@@ -256,35 +254,51 @@ export default {
   color: white;
 }
 
-
-#home-img, #home-img2 {
-    -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
-       -moz-animation: fadein 2s; /* Firefox < 16 */
-        -ms-animation: fadein 2s; /* Internet Explorer */
-         -o-animation: fadein 2s; /* Opera < 12.1 */
-            animation: fadein 2s;
+#home-img,
+#home-img2 {
+  -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
+  -moz-animation: fadein 2s; /* Firefox < 16 */
+  -ms-animation: fadein 2s; /* Internet Explorer */
+  -o-animation: fadein 2s; /* Opera < 12.1 */
+  animation: fadein 2s;
 }
- 
+
 @keyframes fadein {
-    from { opacity: 0; }
-    to   { opacity: 1; }
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 
 /* Firefox < 16 */
 @-moz-keyframes fadein {
-    from { opacity: 0; }
-    to   { opacity: 1; }
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 
 /* Safari, Chrome and Opera > 12.1 */
 @-webkit-keyframes fadein {
-    from { opacity: 0; }
-    to   { opacity: 1; }
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 
 /* Internet Explorer */
 @-ms-keyframes fadein {
-    from { opacity: 0; }
-    to   { opacity: 1; }
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 </style>
